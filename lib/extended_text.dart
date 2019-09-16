aaf6d18f04b2cca4a48ac2ae57579b227e89aa20
Timothy <ygmpkk@gmail.com>
2019-09-17 07:24:27 +0800

    update

--- lib/extended_text.dart
+++ lib/extended_text.dart
@@ -1,6 +1,9 @@
 library extended_text;
 
 export 'src/extended_text.dart';
+export 'src/extended_rich_text.dart';
+export 'src/selection/extended_text_selection.dart';
+export 'src/selection/extended_text_selection_pointer_handler.dart';
 export 'src/over_flow_text_span.dart';
 export 'src/extended_render_paragraph.dart';
 export 'src/selection/extended_text_selection_pointer_handler.dart';
